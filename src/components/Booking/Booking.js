f1b6fb0802a559cff1859218499ec0dd94f9f921
Eman Curmi <emancurmi@gmail.com>
2021-05-24 03:34:37 -0400

    admin update

---
+++ src/components/Booking/Booking.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+export default class Booking extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            studentid: props.booking.studentid,
+            sfirstname: props.booking.sfirstname,
+            slastname: props.booking.slastname,
+            saddress: props.booking.slastname,
+            gnationalid: props.booking.gnationalid,
+            gfirstname: props.booking.gfirstname,
+            glastname: props.booking.glastname,
+            gaddress: props.booking.gaddress,
+            gemail: props.booking.gemail,
+            gtelno: props.booking.gtelno,
+            gmobno: props.booking.gmobno,
+            pickupadd: props.booking.pickupadd,
+            schoolname: props.booking.schoolname,
+            classname: props.booking.classname,
+            pickupdays: props.booking.pickupdays,
+            additionalinfo: props.booking.additionalinfo,
+            fsta: props.booking.fsta,
+            stsa: props.booking.stsa,
+            agreement: props.booking.agreement,
+            current_date: props.booking.current_date,
+            state: props.booking.state
+        }
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return (
+                <p>Loading Form</p>
+            );
+        }
+        else {
+            return (
+                <tr>
+                    <td>{this.state.studentid}</td>
+                    <td>{this.state.sfirstname}</td>
+                    <td>{this.state.slastname}</td>
+                    <td>{this.state.gnationalid}</td>
+                    <td>{this.state.gfirstname}</td>
+                    <td>{this.state.glastname}</td>
+                    <td>{this.state.gaddress}</td>
+                    <td>{this.state.gemail}</td>
+                    <td>{this.state.gtelno}</td>
+                    <td>{this.state.gmobno}</td>
+                    <td>{this.state.pickupadd}</td>
+                    <td>{this.state.schoolname}</td>
+                    <td>{this.state.classname}</td>
+                    <td>{this.state.pickupdays}</td>
+                    <td>{this.state.additionalinfo}</td>
+                    <td>{this.state.fsta}</td>
+                    <td>{this.state.stsa}</td>
+                    <td>{this.state.agreement}</td>
+                    <td>{this.state.current_date}</td>
+                </tr>
+            )
+        }
+    }
+}
---
+++ src/components/Bookings/Bookings.js
@@ -0,0 +1,108 @@
+﻿import React, { Component } from 'react';
+import Booking from '../Booking/Booking';
+import config from '../../config';
+import ReactHTMLTableToExcel from 'react-html-table-to-excel';  
+
+class Bookings extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userid: 0,
+            bookings: [],
+            error: null,
+            isLoading: true,
+            showModal: false
+        }
+    }
+
+    setIsLoading = data => {
+        this.setState({
+            isLoading: data
+        })
+    }
+
+    setBookingData = data => {
+        this.setState({
+            bookings: data
+        })
+    }
+
+    fetchbookings = () => {
+        this.setIsLoading(true);
+
+        fetch(config.API_ENDPOINT + 'booking', {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `Bearer ${config.API_TOKEN}`
+            }
+        })
+            .then(res => {
+                if (!res.ok) {
+                    return res.json().then(error => Promise.reject(error))
+                }
+                return res.json()
+            })
+            .then(data => {
+                this.setBookingData(data)
+            })
+            .catch(error => {
+                this.setState({ error })
+            })
+
+        this.setIsLoading(false);
+    }
+
+
+    componentDidMount() {
+        this.fetchbookings();
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return (
+                <p>Loading Form</p>
+            );
+        }
+        else {
+            return (
+                <table class="table" id="t01">
+                    <thead>
+                        <tr>
+                            <th>Student ID</th>
+                            <th>Student First Name</th>
+                            <th>Student Last Name</th>
+                            <th>Guardian ID</th>
+                            <th>Guardian First Name</th>
+                            <th>Guardian Last Name</th>
+                            <th>Guardian Address</th>
+                            <th>Guardian Email</th>
+                            <th>Guardian Tel No</th>
+                            <th>Guardian Mob No</th>
+                            <th>Pickup Address</th>
+                            <th>School Name</th>
+                            <th>Class Name</th>
+                            <th>Pickup Days</th>
+                            <th>Additional Info</th>
+                            <th>FSTA</th>
+                            <th>STSA</th>
+                            <th>Agreement</th>
+                            <th>Date Signed</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.bookings.map((booking, key) => (
+                            <Booking key={key} booking={booking}/>
+                            ))}
+                    </tbody>
+                    <tfoot>
+                        <ReactHTMLTableToExcel className="btn btn-info" table="t01" filename="ReportExcel" sheet="Sheet" buttonText="Export excel" />
+                    </tfoot>
+                </table>
+            )
+        }
+    }
+} 
+
+export default Bookings;
