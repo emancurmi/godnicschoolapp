64edde2c9557f052af6f2851794127f6b9793bb0
Eman Curmi <emancurmi@gmail.com>
2023-02-28 15:11:31 +0000

    GUI Update

--- src/App.js
+++ src/App.js
@@ -1,45 +1,170 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, { useState, useEffect, useMemo } from "react";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+// react-router components
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
-import Index from './pages/index.js';
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Icon from "@mui/material/Icon";
 
-import Admin from './pages/admin';
-import Member from './pages/member';
-import TermsConditions from './pages/terms_conditions';
-import CookiePolicy from './pages/cookie_policy';
-import PrivacyPolicy from './pages/privacy_policy';
+//  School Bus Booking System React components
+import MDBox from "components/MDBox";
 
-class App extends Component {
-    render() {
-        return (
-            <BrowserRouter>
+//  School Bus Booking System React example components
+import Sidenav from "examples/Sidenav";
+import Configurator from "examples/Configurator";
 
-                <Header />
+//  School Bus Booking System React themes
+import theme from "assets/theme";
+import themeRTL from "assets/theme/theme-rtl";
 
-                <div className='App'>
+//  School Bus Booking System React Dark Mode themes
+import themeDark from "assets/theme-dark";
+import themeDarkRTL from "assets/theme-dark/theme-rtl";
 
-                    <Route exact path="/" component={Index} />
+// RTL plugins
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
-                    <Route path="/member" component={Member} />
+//  School Bus Booking System React routes
+import routes from "routes";
 
-                    <Route path="/admin" component={Admin} />
+//  School Bus Booking System React contexts
+import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "context";
 
-                    <Route path="/termsconditions" component={TermsConditions} />
+// Images
+import brandWhite from "assets/images/logo-ct.png";
+import brandDark from "assets/images/logo-ct-dark.png";
 
-                    <Route path="/cookiepolicy" component={CookiePolicy} />
+class App extends React.Component() {
 
-                    <Route path="/privacypolicy" component={PrivacyPolicy} />
+  render() {
+   // const [controller, dispatch] = useMaterialUIController();
+    /*const {
+      miniSidenav,
+      direction,
+      layout,
+      openConfigurator,
+      sidenavColor,
+      transparentSidenav,
+      whiteSidenav,
+      darkMode,
+    } = controller;*/
+    // const [onMouseEnter, setOnMouseEnter] = useState(false);
+    // const [rtlCache, setRtlCache] = useState(null);
+    // const { pathname } = useLocation();
 
-                </div>
+    const [isLoggedIn, setIsLoggedIn] = useState(
+      () => localStorage.getItem('logged_user') !== null
+    );
 
-                <Footer />
+    /*// Cache for the rtl
+    useMemo(() => {
+      const cacheRtl = createCache({
+        key: "rtl",
+        stylisPlugins: [rtlPlugin],
+      });
+  
+      setRtlCache(cacheRtl);
+    }, []);
+  
+    // Open sidenav when mouse enter on mini sidenav
+    const handleOnMouseEnter = () => {
+      if (miniSidenav && !onMouseEnter) {
+        setMiniSidenav(dispatch, false);
+        setOnMouseEnter(true);
+      }
+    };
+  
+    // Close sidenav when mouse leave mini sidenav
+    const handleOnMouseLeave = () => {
+      if (onMouseEnter) {
+        setMiniSidenav(dispatch, true);
+        setOnMouseEnter(false);
+      }
+    };
+  
+    // Change the openConfigurator state
+    const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
+  
+    // Setting the dir attribute for the body element
+    useEffect(() => {
+      document.body.setAttribute("dir", direction);
+    }, [direction]);
+  
+    // Setting page scroll to 0 when changing the route
+    useEffect(() => {
+      document.documentElement.scrollTop = 0;
+      document.scrollingElement.scrollTop = 0;
+    }, [pathname]);*/
 
-            </BrowserRouter>
-        );
-    }
+    const getRoutes = (allRoutes) =>
+      allRoutes.map((route) => {
+        if (route.collapse) {
+          return getRoutes(route.collapse);
+        }
+
+        if (route.route) {
+          return <Route exact path={route.route} element={route.component} key={route.key} />;
+        }
+
+        return null;
+      });
+
+
+    return isLoggedIn !== false ? (
+      <ThemeProvider theme={darkMode ? themeDark : theme}>
+        <CssBaseline />
+        {layout === "dashboard" && (
+          <>
+            <Sidenav
+              color={sidenavColor}
+              brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+              brandName=" School Bus Booking"
+              routes={routes}
+            //onMouseEnter={handleOnMouseEnter}
+            //onMouseLeave={handleOnMouseLeave}
+            />
+            {/*<Configurator />
+          {configsButton}*/}
+          </>
+        )}
+
+        {layout === "vr" && <Configurator />}
+        <Routes>
+          {getRoutes(routes)}
+          <Route path="*" element={<Navigate to="/tables" />} />
+        </Routes>
+
+      </ThemeProvider>
+    ) : (
+      <ThemeProvider theme={darkMode ? themeDark : theme}>
+        <CssBaseline />
+        {layout === "dashboard" && (
+          <>
+            <Sidenav
+              color={sidenavColor}
+              brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+              brandName=" School Bus Booking"
+              routes={routes}
+            //onMouseEnter={handleOnMouseEnter}
+            //onMouseLeave={handleOnMouseLeave}
+            />
+            {/*<Configurator />
+          {configsButton}*/}
+          </>
+        )}
+
+        {layout === "vr" && <Configurator />}
+        <Routes>
+          {getRoutes(routes)}
+          <Route path="*" element={<Navigate to="/authentication/sign-in" />} />
+        </Routes>
+
+      </ThemeProvider>
+    );
+  }
 }
-
 export default App;
